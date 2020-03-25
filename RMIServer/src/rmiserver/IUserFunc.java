38ec5f583c2ff95d0e4dad244f71ce3780217fda
BpHana <57785153+BpHana@users.noreply.github.com>
2020-03-25 23:38:24 +0700

    revert

--- RMIServer/src/rmiserver/IUserFunc.java
+++ RMIServer/src/rmiserver/IUserFunc.java
@@ -19,15 +19,9 @@ public interface IUserFunc extends Remote {
     public int createUser(String username, String password, String fullname, String gender, String email, String phone, String address) throws RemoteException;
 
     public User deposit(User oldUserInfo, int depositAmount) throws RemoteException;
-<<<<<<< HEAD
-
+    
     public int withdraw() throws RemoteException;
 
-=======
-
-    public int withdraw() throws RemoteException;
-
->>>>>>> parent of 6b62d43... Merge branch 'master' into Thang
     public int transfer() throws RemoteException;
 
     public int changeInfo() throws RemoteException;
