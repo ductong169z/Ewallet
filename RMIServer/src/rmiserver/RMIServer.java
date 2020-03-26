21d8ba9d1239d6b6bcafc49932eff410f038c07b
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-03-26 19:01:31 +0700

    Update RMIServer.java

--- RMIServer/src/rmiserver/RMIServer.java
+++ RMIServer/src/rmiserver/RMIServer.java
@@ -26,18 +26,19 @@ public class RMIServer {
 
     public static void main(String[] args) {
         try {
-            // create 3 ports
+            /* Create 3 Remote Registries for 3 Ports */
             LocateRegistry.createRegistry(69);
             LocateRegistry.createRegistry(70);
             LocateRegistry.createRegistry(71);
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=Ewallet", "sa", "123456");
-            // bind 3 classes to 3 ports with equal name
+            
+            /* Associate 3 names for corresponding 3 Remote Objects */
             Naming.bind("rmi://localhost:69/Authentication", new Authentication(conn)); // for authenticating login sessions
             Naming.bind("rmi://localhost:70/UserFunctions", new UserFunc(conn)); // for user functions
             Naming.bind("rmi://localhost:71/AdminFunctions", new AdminFunc(conn)); // for admin functions
 
             System.out.println("Server Started Successfully! ...");
-        }catch (MalformedURLException | AlreadyBoundException | RemoteException | SQLException ex) {
+        } catch (MalformedURLException | AlreadyBoundException | RemoteException | SQLException ex) {
             Logger.getLogger(RMIServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
